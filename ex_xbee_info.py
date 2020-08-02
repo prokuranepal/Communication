d9a41a2cd547ef8d5e8e7532ba5f374cd3a18b97
Abhish Khanal <abheeshkhanal@gmail.com>
2020-08-02 11:38:25 +0545

    read gcs xbee info

---
+++ ex_xbee_info.py
@@ -0,0 +1,17 @@
+from digi.xbee.devices import XBeeDevice
+PORT = '/dev/ttyUSB0'
+BAUD_RATE = 9600
+
+device = XBeeDevice(PORT, BAUD_RATE)
+device.open()
+
+# Get the 64-bit address of the device.
+addr_64 = device.get_64bit_addr()
+# Get the node identifier of the device.
+node_id = device.get_node_id()
+# Get the hardware version of the device.
+hardware_version = device.get_hardware_version()
+# Get the firmware version of the device.
+firmware_version = device.get_firmware_version()
+
+print("MAC ADDRESS = ",addr_64)

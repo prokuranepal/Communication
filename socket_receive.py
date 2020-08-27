a482eed1a7850deaf329a8c0a791f1c303b65581
Abhish Khanal <abheeshkhanal@gmail.com>
2020-08-27 13:35:58 +0545

    send mavlink message

--- socket_receive.py
+++ socket_receive.py
@@ -1,40 +1,19 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 14550
-sock = socket.socket(socket.AF_INET,  # Internet
+
+UDP_PORT_RX = 14550
+UDP_PORT_TX = 14555
+
+sock_rx = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
-sock.bind((UDP_IP, UDP_PORT))
+sock_rx.bind((UDP_IP, UDP_PORT_RX))
+
+sock_tx = socket.socket(socket.AF_INET,
+                        socket.SOCK_DGRAM)
+
+
 while True:
-    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    data, addr = sock_rx.recvfrom(1024)  # buffer size is 1024 bytes
     print("received message: %s" % data)
-
-# import socket
-# import pickle
-
-
-# class ProcessData:
-#     process_id = 0
-#     project_id = 0
-#     task_id = 0
-#     start_time = 0
-#     end_time = 0
-#     user_id = 0
-#     weekend_id = 0
-
-
-# HOST = 'localhost'
-# PORT = 14550
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((HOST, PORT))
-# s.listen(1)
-
-# # Access the information by doing data_variable.process_id or data_variable.task_id etc..,
-# # print 'Data received from client'
-# while True:
-#     conn, addr = s.accept()
-#     # print 'Connected by', addr
-#     data = conn.recv(4096)
-#     data_variable = pickle.loads(data)
-#     conn.close()
-#     print(data_variable)
+    sock_tx.sendto(data,(UDP_IP,UDP_PORT_TX))
--- socket_send.py
+++ socket_send.py
@@ -1,43 +1,70 @@
 import socket
-import time
+import serial, time
+import threading
+import queue
 UDP_IP = "127.0.0.1"
 UDP_PORT = 14550
-MESSAGE = b"Hello, World!"
+MESSAGE = b"mavlink message"
+
 
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 print("message: %s" % MESSAGE)
-  
-sock = socket.socket(socket.AF_INET, # Internet
-                       socket.SOCK_DGRAM) # UDP
-while True:
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    time.sleep(2)
-# import socket
-# import pickle
+
+class HandleServer():
+    def __init__(self,udp_ip,udp_port):
+        self.UDP_IP = udp_ip
+        self.UDP_PORT = udp_port
+        self.sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+        #self.sock.bind((self.UDP_IP, self.UDP_PORT))
+
+        self.mavlink_queue = queue.Queue()
+
+        receive_thread = threading.Thread(target= self.receive_data, daemon = True)
+        receive_thread.start()
+
+    def send_data(self,data):
+        self.sock.sendto(data,(self.UDP_IP,self.UDP_PORT))
+
+    def receive_data(self):
+        while True:
+            data,_ = self.sock.recvfrom(1024)
+            self.mavlink_queue.put(data)
 
 
-# class ProcessData:
-#     process_id = 0
-#     project_id = 0
-#     task_id = 0
-#     start_time = 0
-#     end_time = 0
-#     user_id = 0
-#     weekend_id = 0
+def do_mavlink_send(drone,check_gcs):
+    while True:
+        if 100 <= drone.in_waiting < 4095:
+            data = drone.read(drone.in_waiting)
+            while data:
+                print('[Drone -> Internet] {}'.format(data[:255]))
+                check_gcs.send_data(data[:255])
+                data = data[255:]
+        elif drone.in_waiting == 4095:
+            drone.reset_input_buffer()
+        else:
+            pass
+
+def do_mavlink_receive(drone,check_gcs):
+    while True:
+        while not check_gcs.mavlink_queue.empty():
+            drone.write(check_gcs.mavlink_queue.get())
+
+def main():
+    drone = serial.Serial('/dev/ttyACM0',115200)
+    check_gcs = HandleServer(udp_ip=UDP_IP,udp_port=UDP_PORT)
+    
+    mavlink_send_thread = threading.Thread(target= do_mavlink_send, args = (drone,check_gcs,),daemon = True)
+    mavlink_send_thread.start()
+    
+    mavlink_rx_thread = threading.Thread(target=do_mavlink_receive,args=(drone,check_gcs,),daemon = True)
+    mavlink_rx_thread.start()
+
+    input()
 
 
-# HOST = 'localhost'
-# PORT = 14550
-# # Create a socket connection.
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((HOST, PORT))
 
-# # Create an instance of ProcessData() to send to server.
-# variable = ProcessData()
-# # Pickle the object and send it to the server
-# data_string = pickle.dumps(variable)
-# s.send(data_string)
-
-# s.close()
+if __name__ == '__main__':
+    main()

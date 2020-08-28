d4179c15e10e6d4568e563d66d397b3b03344622
Abhish Khanal <abheeshkhanal@gmail.com>
2020-08-28 20:13:46 +0545

    Sends mavlink msg to server

--- socket_receive.py
+++ socket_receive.py
@@ -1,19 +1,17 @@
 import socket
+from socketIO_client_nexus import SocketIO, BaseNamespace
+import json
 
-UDP_IP = "127.0.0.1"
+def receive_data(var):
+    print(var)
 
-UDP_PORT_RX = 14550
-UDP_PORT_TX = 14555
-
-sock_rx = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
-sock_rx.bind((UDP_IP, UDP_PORT_RX))
-
-sock_tx = socket.socket(socket.AF_INET,
-                        socket.SOCK_DGRAM)
+socket = SocketIO('https://nicwebpage.herokuapp.com', verify =True)
+socket_a = socket.define(BaseNamespace,'/JT601')
+socket_a.emit("joinPi")
+print("conected to server")
 
 
 while True:
-    data, addr = sock_rx.recvfrom(1024)  # buffer size is 1024 bytes
-    print("received message: %s" % data)
-    sock_tx.sendto(data,(UDP_IP,UDP_PORT_TX))
+    socket_a.on("LAND",receive_data)
+    #socket_a.emit("homePosition",i)
+    socket.wait(seconds=0.01)
--- socket_send.py
+++ socket_send.py
@@ -2,6 +2,7 @@ import socket
 import serial, time
 import threading
 import queue
+from socketIO_client_nexus import SocketIO, BaseNamespace
 UDP_IP = "127.0.0.1"
 UDP_PORT = 14550
 MESSAGE = b"mavlink message"
@@ -14,43 +15,48 @@ print("message: %s" % MESSAGE)
 
 class HandleServer():
     def __init__(self,udp_ip,udp_port):
-        self.UDP_IP = udp_ip
-        self.UDP_PORT = udp_port
-        self.sock = socket.socket(socket.AF_INET, # Internet
-                        socket.SOCK_DGRAM) # UDP
-        #self.sock.bind((self.UDP_IP, self.UDP_PORT))
+        # self.UDP_IP = udp_ip
+        # self.UDP_PORT = udp_port
+        # self.sock = socket.socket(socket.AF_INET, # Internet
+        #                 socket.SOCK_DGRAM) # UDP
+        # #self.sock.bind((self.UDP_IP, self.UDP_PORT))
+        self.socket = SocketIO('https://nicwebpage.herokuapp.com', verify =True)
+        self.socket_a = self.socket.define(BaseNamespace,'/JT601')
+        self.socket_a.emit("joinWebsite")
 
         self.mavlink_queue = queue.Queue()
 
-        receive_thread = threading.Thread(target= self.receive_data, daemon = True)
-        receive_thread.start()
+        # receive_thread = threading.Thread(target= self.receive_data, daemon = True)
+        # receive_thread.start()
 
     def send_data(self,data):
-        self.sock.sendto(data,(self.UDP_IP,self.UDP_PORT))
+        #self.sock.sendto(data,(self.UDP_IP,self.UDP_PORT))
+        self.socket_a.emit("LAND",str(data))
+        self.socket.wait(seconds=0.01)
 
-    def receive_data(self):
-        while True:
-            data,_ = self.sock.recvfrom(1024)
-            self.mavlink_queue.put(data)
+    # def receive_data(self):
+    #     while True:
+    #         data,_ = self.sock.recvfrom(1024)
+    #         self.mavlink_queue.put(data)
 
 
 def do_mavlink_send(drone,check_gcs):
     while True:
-        if 100 <= drone.in_waiting < 4095:
+        if 100 <= drone.in_waiting < 4095:  
             data = drone.read(drone.in_waiting)
             while data:
-                print('[Drone -> Internet] {}'.format(data[:255]))
-                check_gcs.send_data(data[:255])
-                data = data[255:]
+                print('[Drone -> Internet] {}'.format(data))
+                #check_gcs.send_data(data[:255])
+                #data = data[255:]
         elif drone.in_waiting == 4095:
             drone.reset_input_buffer()
         else:
             pass
 
-def do_mavlink_receive(drone,check_gcs):
-    while True:
-        while not check_gcs.mavlink_queue.empty():
-            drone.write(check_gcs.mavlink_queue.get())
+# def do_mavlink_receive(drone,check_gcs):
+#     while True:
+#         while not check_gcs.mavlink_queue.empty():
+#             drone.write(check_gcs.mavlink_queue.get())
 
 def main():
     drone = serial.Serial('/dev/ttyACM0',115200)
@@ -59,8 +65,8 @@ def main():
     mavlink_send_thread = threading.Thread(target= do_mavlink_send, args = (drone,check_gcs,),daemon = True)
     mavlink_send_thread.start()
     
-    mavlink_rx_thread = threading.Thread(target=do_mavlink_receive,args=(drone,check_gcs,),daemon = True)
-    mavlink_rx_thread.start()
+    # mavlink_rx_thread = threading.Thread(target=do_mavlink_receive,args=(drone,check_gcs,),daemon = True)
+    # mavlink_rx_thread.start()
 
     input()
 

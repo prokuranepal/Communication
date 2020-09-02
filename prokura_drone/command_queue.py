09caee0a2b61a109f51d6322fb4b275d94e13412
Abhish Khanal <abheeshkhanal@gmail.com>
2020-09-02 16:17:03 +0545

    queue class add for incoming command

---
+++ prokura_drone/command_queue.py
@@ -0,0 +1,16 @@
+import queue as Queue
+
+class CommandQueue(Queue.Queue):
+    
+    def _init(self, maxsize):
+        self.queue = set()
+
+    def _put(self, item):
+        self.queue.add(item)
+
+    def _get(self):
+        return self.queue.pop()
+
+    def __contains__(self, item):
+        with self.mutex:
+            return item in self.queue
